1d0115adf8ab280d483f3a03005efaa4a9ee9629
ducchinh <hoangducchinh730@gmail.com>
2025-12-17 19:00:21 +0700

    feat(entity): add Address embeddable class
    - Define Address as @Embeddable for Hibernate/JPA
    - Include fields: street, city, zipcode
    - Provide constructors, getters and setters
    - Enables embedding Address into User entity without separate table

---
+++ LibrarySystem/src/main/java/com/chinh/Address.java
@@ -0,0 +1,42 @@
+package com.chinh;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String street;
+    private String zipcode;
+    private String city;
+
+    public Address() {};
+    public Address(String street, String city, String zipcode)
+    {
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
