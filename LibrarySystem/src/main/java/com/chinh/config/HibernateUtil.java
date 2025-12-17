6dde06a76485945215945020d29fa108a19154a2
ducchinh <hoangducchinh730@gmail.com>
2025-12-17 20:55:17 +0700

    Add the HibernateUtil - have a static to save the single SessionFactory for the app, a static method to getSessionnFactory. So we don't need to create new one whenever we need to open a session. We also have a method to close the SessionFactory.

---
+++ LibrarySystem/src/main/java/com/chinh/config/HibernateUtil.java
@@ -0,0 +1,30 @@
+package com.chinh.config;
+
+import com.chinh.Book;
+import com.chinh.LibraryCard;
+import com.chinh.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory; // Vì biến này không thuộc về một instance
+
+    public static SessionFactory getSessionFactory()
+    {
+        if (sessionFactory == null)
+        {
+            sessionFactory = new Configuration()
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Book.class)
+                .addAnnotatedClass(LibraryCard.class)
+                .buildSessionFactory();
+
+        }
+        return sessionFactory;
+    }
+
+    public void shutdown()
+    {
+        sessionFactory.close();
+    }
+}
