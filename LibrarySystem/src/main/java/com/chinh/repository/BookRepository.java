5269039386c48134d3b67cf8c0fe3ca6ef2d12b0
ducchinh <hoangducchinh730@gmail.com>
2025-12-17 21:14:38 +0700

    - Use the try-with-resource so the SessionFactory wil close if errors occur or finish the method. - Change the type from Query to Query<Book>. - Temporarily delete with remove method. We will fix this to ensure the integrity later.

--- LibrarySystem/src/main/java/com/chinh/repository/BookRepository.java
+++ LibrarySystem/src/main/java/com/chinh/repository/BookRepository.java
@@ -10,50 +10,44 @@ import org.hibernate.query.Query;
 import java.util.List;
 
 public class BookRepository {
+
     public void save(Book book)
     {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-
-        session.merge(book);
-
-        tx.commit();
-        session.close();
+        try(Session session = HibernateUtil.getSessionFactory().openSession())
+        {
+            Transaction tx = session.beginTransaction();
+            session.merge(book);
+            tx.commit();
+        }
     }
 
     public Book findById(int bookId)
     {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-        Book book = session.find(Book.class, bookId);
-
-        session.close();
-
-        return book;
+        try(Session session = HibernateUtil.getSessionFactory().openSession())
+        {
+            return session.find(Book.class, bookId);
+        }
     }
 
     public List<Book> findAll()
     {
-        String hql = "FROM Book";
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-        Query query = session.createQuery(hql, Book.class);
-        List<Book> bookList = query.getResultList();
-
-        session.close();
-        return  bookList;
+        try(Session session = HibernateUtil.getSessionFactory().openSession())
+        {
+            String hql = "FROM Book";
+            Query<Book> query = session.createQuery(hql, Book.class);
+            return query.getResultList();
+        }
     }
 
     public void delete(int bookId)
     {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-
-        Book bookToDelete = session.find(Book.class, bookId);
-        if (bookToDelete == null) return;
-        session.remove(bookToDelete);
-
-        tx.commit();
-        session.close();
+        try(Session session = HibernateUtil.getSessionFactory().openSession())
+        {
+            Transaction tx = session.beginTransaction();
+            Book bookToDelete = session.find(Book.class, bookId);
+            if(bookToDelete != null)
+                session.remove(bookToDelete);
+            tx.commit();
+        }
     }
 }
