a62b09561361c1d323107408e885d1649792ba16
ducchinh <hoangducchinh730@gmail.com>
2025-12-18 11:05:57 +0700

    stuff

--- Hibernate/src/main/java/com/chinh/Main.java
+++ Hibernate/src/main/java/com/chinh/Main.java
@@ -8,14 +8,9 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setRollNo(4);
-        s1.setsName("Hùng");
-        s1.setAge(13);
-
-//        Configuration cfg = new Configuration();
-//        cfg.addAnnotatedClass(com.chinh.Student.class);
-//        cfg.configure();
-//        SessionFactory sf = cfg.buildSessionFactory();
+        s1.setRollNo(1);
+        s1.setsName("Chinh");
+        s1.setAge(10);
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.chinh.Student.class)
@@ -24,13 +19,13 @@ public class Main {
 
         Session session = sf.openSession();
 
-        Student s2 = session.find(Student.class, 8);
+        session.merge(s1);
 
         session.close();
         sf.close();
 
-        if (s2 != null)
-            System.out.println(s2.getsName());
+        if (s1 != null)
+            System.out.println(s1.getsName());
         else
             System.err.println("Không tồn tại trong database.");
     }
